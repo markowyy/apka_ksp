efea56c77a8209430937f80842336314e4e4f286
Matrer <koziol.marcin96@gmail.com>
2020-07-01 21:41:00 +0200

    *code refactor *add singleton Socket *add connect/disconnect button *add editText for Address to MainActivity *small changes in chart

--- app/src/main/java/com/example/apka_ksp/ChartActivity.kt
+++ app/src/main/java/com/example/apka_ksp/ChartActivity.kt
@@ -2,36 +2,43 @@ package com.example.apka_ksp
 
 import android.app.Activity
 import android.os.Bundle
-import com.github.nkzawa.socketio.client.IO
+import com.example.apka_ksp.SocketIO.Companion.getValueFromJSON
 import com.github.nkzawa.socketio.client.Socket
 import com.jjoe64.graphview.DefaultLabelFormatter
 import com.jjoe64.graphview.Viewport
 import com.jjoe64.graphview.series.DataPoint
 import com.jjoe64.graphview.series.LineGraphSeries
 import kotlinx.android.synthetic.main.chart.*
-import org.json.JSONObject
 
-class Chart : Activity() {
-    private var socket: Socket = IO.socket("https://ksp-project.azurewebsites.net/")
+class ChartActivity : Activity() {
+    private var socket: Socket = SocketIO.instance.getSocket()
     private lateinit var series: LineGraphSeries<DataPoint>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.chart)
-
-        socket.connect()
-        socket.emit("confirmation", true)
         socket.on("currentValue") { args ->
-            val data = args[0] as JSONObject
-            val value = data.getDouble("value")
-            addEntry(value)
+            addEntry(getValueFromJSON(args))
         }
 
         series = LineGraphSeries()
+        series.isDrawDataPoints = true
         graph.addSeries(series)
 
+        addValuesFromMainActivity()
         formatLabel()
         customizeGraphViewport(graph.viewport)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        addValuesFromMainActivity()
+    }
+
+    private fun addValuesFromMainActivity() {
+        SocketIO.instance.listOfValues.forEach {
+            addEntry(it)
+        }
     }
 
     private fun formatLabel() {
@@ -50,18 +57,21 @@ class Chart : Activity() {
         viewport.computeScroll()
         viewport.isScalable = true
         viewport.isScrollable = true
-        viewport.isYAxisBoundsManual = false
+        viewport.isYAxisBoundsManual = true
         viewport.isXAxisBoundsManual = true
         viewport.setMinY(0.0)
         viewport.setMaxX(20.0)
         viewport.setMinX(0.0)
-        viewport.setMaxY(35.0)
+        viewport.setMaxY(50.0)
     }
 
     private var lastX = 0.0
     private fun addEntry(value: Double) {
         runOnUiThread {
-            series.appendData(DataPoint(lastX++, value), true, 30)
+            series.appendData(DataPoint(lastX++, value), false, 20)
+            if (lastX >= 20.0) {
+                graph.viewport.scrollToEnd()
+            }
         }
     }
 }
--- app/src/main/java/com/example/apka_ksp/MainActivity.kt
+++ app/src/main/java/com/example/apka_ksp/MainActivity.kt
@@ -3,30 +3,91 @@ package com.example.apka_ksp
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import com.github.nkzawa.socketio.client.IO
+import android.widget.Toast
+import com.example.apka_ksp.SocketIO.Companion.getValueFromJSON
 import com.github.nkzawa.socketio.client.Socket
 import kotlinx.android.synthetic.main.activity_main.*
-import org.json.JSONObject
 
 class MainActivity : Activity() {
-    private var socket: Socket = IO.socket("https://ksp-project.azurewebsites.net/")
+    private var socket: Socket? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        socket.connect()
-        socket.emit("confirmation", true)
 
-        socket.on("currentValue") { args ->
-            val data = args[0] as JSONObject
-            val value = data.getString("value")
-            runOnUiThread {
-                tempid.text = "$value℃"
+        buttonChart.setOnClickListener {
+            if (socket == null) {
+                Toast.makeText(this, "You are not connected", Toast.LENGTH_SHORT).show()
+            } else {
+                openChartActivity()
             }
         }
-        buttonChart.setOnClickListener {
-            val i = Intent(this@MainActivity, Chart::class.java)
-            startActivity(i)
+        buttonConnect.setOnClickListener {
+            if (buttonConnect.text == "Connect") {
+                connectToServer()
+            } else {
+                socket?.disconnect()
+            }
         }
     }
+
+    private fun openChartActivity() {
+        val intent = Intent(this, ChartActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun connectToServer() {
+        socket = SocketIO.instance.createSocket(editTextAddress.text.toString())
+        if (socket == null) {
+            Toast.makeText(this, "Cannot connect", Toast.LENGTH_SHORT).show()
+        } else {
+            socket?.connect()
+            socket?.on(Socket.EVENT_CONNECT) {
+                onEventConnect()
+            }?.on(Socket.EVENT_DISCONNECT) {
+                onEventDisconnect()
+            }
+        }
+    }
+
+    private fun onEventDisconnect() {
+        runOnUiThread {
+            Toast.makeText(this, "Disconnected", Toast.LENGTH_SHORT).show()
+            buttonConnect.text = "Connect"
+            buttonChart.isEnabled = false
+        }
+        SocketIO.instance.listOfValues.clear()
+    }
+
+    private fun onEventConnect() {
+        //TODO chyba tu trzeba wrzucić create room ale dodało mi kilka pokoi na raz :/
+        // socket?.emit("createRoom", "roomName")
+        runOnUiThread {
+            Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show()
+            buttonConnect.text = "Disconnect"
+            buttonChart.isEnabled = true
+        }
+        addCurrentValueListener()
+    }
+
+    private fun addCurrentValueListener() {
+        socket?.on("currentValue") { args ->
+            socket?.emit("confirmation", true)
+            val value = getValueFromJSON(args)
+            SocketIO.instance.listOfValues.add(value)
+            drawValue(value)
+        }
+    }
+
+    private fun drawValue(value: Double) {
+        runOnUiThread {
+            if (isWhole(value)) {
+                textViewTemp.text = "${value.toInt()}℃"
+            } else {
+                textViewTemp.text = "$value℃"
+            }
+        }
+    }
+
+    private fun isWhole(value: Double) = value - value.toInt() == 0.0
 }
